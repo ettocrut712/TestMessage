0dea0c9cc499785f3729a8f467ecfb820721114f
normandturcotte712@gmail.com <ysee03@outlook.com>
2019-03-22 12:00:59 -0400

    22 mars texte original avec "texte ajouté"

---
+++ TestMessage/ClassO2.cpp
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+#include "ClassO2.h"
+
+
+ClassO2::ClassO2()
+{
+}
+
+
+ClassO2::~ClassO2()
+{
+}
+
+ClassO1::ClassO1()
+{
+}
+
+
+ClassO1::~ClassO1()
+{
+}
+
+void ClassO2::jeTravaillePourO2()
+{
+	cout << "je suis au service de toutes les classes" << endl;
+};
+
+
+ClassO1::ClassO1(ClassO2 *lienO2)
+{
+	this->lienO2 = lienO2;
+
+};
+
+void ClassO1::jeTravaillePourO1()
+{
+
+	lienO2->jeTravaillePourO2();
+};
+
+int ClassO1::uneAutreMethode(int a)
+{
+	return a;
+
+};
---
+++ TestMessage/ClassO2.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class ClassO2
+{
+public:
+	ClassO2();
+	~ClassO2();
+
+private:
+	int unAttribut2;
+	double unAutreAttribut2;
+
+public:
+	void jeTravaillePourO2();
+	
+};
+
+
+class ClassO1
+{
+public:
+	ClassO1();
+	~ClassO1();
+
+private:
+	int unAttribut;
+	int unAutreAttribut;
+	ClassO2 *lienO2;
+
+public:
+
+	ClassO1(ClassO2 *lienO2);
+
+	void jeTravaillePourO1();
+
+	int uneAutreMethode(int a);
+
+};
+
+//22 mars texte ajouté...
+
